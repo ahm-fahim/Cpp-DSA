5dfbf4d62c5db2a255ee8764043ae31cf38f9bc7
ahm-fahim <contactahmfahim@gmail.com>
2023-06-16 07:41:08 +0600

    input output

---
+++ 2.InputOutput/2.1.InputOutput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    cout << "num1 : ";
+    cin >> num1;
+
+    int num2;
+    cout << "\nnum1 : ";
+
+    cin >> num2;
+
+    int sum = num1 + num2;
+    cout << sum << endl;
+
+    return 0;
+}
