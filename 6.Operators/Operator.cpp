e6629daabdfdcd94222493e196103940b2784072
ahm-fahim <contactahmfahim@gmail.com>
2023-06-17 21:30:16 +0600

    pattern

---
+++ 6.Operators/Operator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*Operator > Binary Operator + Unary Operator
+     *             (+,-,*,/,%)       (-- , ++)
+     *                        (increment , decrement)
+     *                 (Preincrement ++a),(PostIncrement a++)
+     *
+     */
+
+    // preincrement
+    int a = 10;
+    cout << "a " << a << endl;
+    int b = ++a;
+    cout << "b " << b << endl;
+
+    // postincrement
+    int c = 20;
+    cout <<"c "<< c << endl;
+    int d = c++;
+    cout <<"d "<< d << endl;
+
+    // relational operator
+    
+}
